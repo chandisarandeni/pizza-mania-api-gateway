ef10d1f08b726a7b4cf1ab4cdb515f92895979af
chandisarandeni <chandisarandeni2003@gmail.com>
2025-09-10 09:04:13 +0530

    init: springbot project with gateway dependency 🚀

---
+++ src/test/java/com/pizzamania/pizza_mania_api_gateway/PizzaManiaApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pizzamania.pizza_mania_api_gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PizzaManiaApiGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
