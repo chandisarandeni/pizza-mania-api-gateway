08ba9902af8ec4e8e0f2d8fd6ce1ad604ee42354
chandisarandeni <chandisarandeni2003@gmail.com>
2025-09-13 20:58:19 +0530

    feat: implement order items  route✅

--- src/main/java/com/pizza_mania_api_gateway/Pizza/Mania/API/Gateway/PizzaManiaApiGatewayApplication.java
+++ src/main/java/com/pizza_mania_api_gateway/Pizza/Mania/API/Gateway/PizzaManiaApiGatewayApplication.java
@@ -39,6 +39,11 @@ public class PizzaManiaApiGatewayApplication {
                         .filters(f -> f.addResponseHeader("X-Response-Header", "PizzaManiaApiGateway"))
                         .uri("http://localhost:8081"))
 
+                //--------------- order-items-------------------------
+                .route(r -> r.path("/api/v1/orderitems/**")
+                        .filters(f -> f.addResponseHeader("X-Response-Header", "PizzaManiaApiGateway"))
+                        .uri("http://localhost:8081"))
+
                 // ----------------- notification-service -----------------
                 .route(r -> r.path("/api/v1/notifications/**")
                         .filters(f -> f.addResponseHeader("X-Response-Header", "PizzaManiaApiGateway"))
